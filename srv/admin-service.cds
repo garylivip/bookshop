4be141ebc8cbf96786975b425933da99f39bd848
Gary Li <garylivip@outlook.com>
2024-11-26 23:04:28 +0800

    Generate Integer primary keys on Books & Authors CREATE & NEW

--- srv/admin-service.cds
+++ srv/admin-service.cds
@@ -5,15 +5,23 @@ using { bookshop.v2024 as bookshop } from '../db/schema';
 @requires: 'admin'
 
 service AdminService {  
+  // The following service definition will be consumed by frond-end applications.
   entity Books as projection on bookshop.Books;
   entity Authors as projection on bookshop.Authors;
-  entity Genres as projection on bookshop.Genres; 
-  action add (x:Integer, y: Integer) returns Integer;
-  // Post  http://localhost:4004/admin/add   body: {"x": 201, "y": 2}
-  function subtract (x: Integer, y: Integer) returns Integer;
-  // Get http://localhost:4004/admin/subtract(x=200,y=101)
 
+  // The following code consists of some additional experimental exercises, which are consumed by Bruno.
+  action add (x:Integer, y: Integer) returns Integer;  
+  function subtract (x: Integer, y: Integer) returns Integer;
 }
 
-//@(requires: 'authenticated-user') 
-// annotate AdminService with @(requires: 'manager');
+/* 
+  The following code demonstrates some alternative methods for annotating.
+  @(requires: 'authenticated-user') 
+  annotate AdminService with @(requires: 'manager');
+  service AdminService @(requires:'admin', path:'/admin') {
+
+  The following code snippets illustrate how to call actions and functions, respectively.
+  calling an action:  Post + http://localhost:4004/admin/add  +  body: {"x": 201, "y": 2}
+  calling an function: Get http://localhost:4004/admin/subtract(x=200,y=101)
+*/
+
--- srv/handlers/admin-service.js
+++ srv/handlers/admin-service.js
@@ -1,31 +1,38 @@
 const cds = require('@sap/cds');
 
-module.exports = class AdminService {
+module.exports = class AdminService extends cds.ApplicationService{
     async init() {
-        this.db = await cds.connect.to('db'); // connect to database service
-        this.Books = this.db.entities.Books; // get reflected definitions
+        this.db = await cds.connect.to('db'); // 明天验证一下，这一句在这里有没有用
+        this.before(['NEW', 'CREATE'], 'Authors', genid); // Multiple events can be passed as an array if they share the same handler
+        this.before(['NEW', 'CREATE'], 'Books', genid);
 
         this.on('add', this.add);
         this.on('subtract', this.subtract);
-        this.on('READ', 'Books', this.readBooks);
+        this.on('READ', 'Books', this.readBooks());
+        
+        return super.init();
     }
 
-    async add(req) {
+    add(req) {
         const { x, y } = req.data;
         return x + y;
     }
 
-    async subtract(req) {
+    subtract(req) {
         const { x, y } = req.data;
         return x - y;
     }
-
-    async readBooks(req) {
-        return await this.db.read(this.Books);
-    }
 }
 
-// module.exports = cds.service.impl(function() {
-//     return new AdminService().init();
-// });
-
+/* 
+Generate Integer primary keys （Books.ID, Authors.ID) for target entity in request      
+const { id } 是 const result = XXX   +   id = result.id 的简写
+SELECT.one.from(req.target).columns('max(ID) as id') 会返回一个对象，对象中有一个属性叫做id
+req.target 是请求的目标实体, 在这里是Authors或者Books
+Increment the max ID by 1, starting from 1 if no ID exists
+*/
+async function genid (req) {
+  if (req.data.ID) return;
+  const { id } = await SELECT.one.from(req.target).columns("max(ID) as id");
+  req.data.ID = id ? id + 1 : 1;
+}
